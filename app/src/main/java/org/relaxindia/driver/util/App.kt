5872d65ba0fbfd1a4ebf102e5f66434c761b102c
soumoq <soumojyotiganguly270@gmail.com>
2022-02-09 02:24:46 +0530

    Implement document upload

--- app/src/main/java/org/relaxindia/driver/util/App.kt
+++ app/src/main/java/org/relaxindia/driver/util/App.kt
@@ -45,6 +45,7 @@ object App {
     const val REJECT_BOOKING: String = "reject-request"
     const val GET_UPLOAD_DOCUMENTS: String = "get-upload-documents"
     const val GET_SCHEDULE_BOOKING : String = "get-schedule-bookings"
+    const val UPLOAD_DOCUMENT = "upload-documents"
 
 
 
--- app/src/main/java/org/relaxindia/driver/view/activity/DocumentActivity.kt
+++ app/src/main/java/org/relaxindia/driver/view/activity/DocumentActivity.kt
@@ -1,5 +1,6 @@
 package org.relaxindia.driver.view.activity
 
+import android.R.attr
 import android.content.Intent
 import android.database.Cursor
 import android.net.Uri
@@ -14,23 +15,40 @@ import org.relaxindia.driver.service.volly.VollyApi
 import org.relaxindia.driver.util.toast
 import android.provider.MediaStore
 import java.io.File
+import android.app.Activity
+import java.io.FileNotFoundException
+import android.provider.MediaStore.MediaColumns
+import android.R.attr.data
+import android.util.Log
+import okhttp3.*
+import org.json.JSONObject
+import org.relaxindia.driver.util.App
+import java.io.IOException
+import java.lang.Exception
+import okhttp3.Route
+
+
+
 
 
 class DocumentActivity : AppCompatActivity() {
+
+    val image = 1
+    val drivingLicence = 2
+    val idProve = 3;
+    val ambulancePaper = 4
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_document)
 
-        document_image_select.setOnClickListener {
-            val intent = Intent()
-                .setType("*/*")
-                .setAction(Intent.ACTION_GET_CONTENT)
-
-            startActivityForResult(Intent.createChooser(intent, "Select a file"), 111)
-        }
-
         VollyApi.getUploadDocument(this)
+
+        document_image_select.setOnClickListener {
+            val photoPickerIntent = Intent(Intent.ACTION_PICK)
+            photoPickerIntent.type = "image/*"
+            startActivityForResult(photoPickerIntent, image)
+        }
 
     }
 
@@ -43,30 +61,81 @@ class DocumentActivity : AppCompatActivity() {
         }
     }
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
         super.onActivityResult(requestCode, resultCode, intentData)
-        if (requestCode == 111 && resultCode == RESULT_OK) {
-            val selectedFile: Uri? = intentData?.data //The uri with the location of the file
-            val file =
-                File("${Environment.getExternalStorageDirectory().absolutePath}${selectedFile?.path}")
-            toast(getPath(selectedFile))
+        if (requestCode > 0) if (resultCode === RESULT_OK) {
+            val selectedImage: Uri = intentData?.data!!
+            val filePath = getPath(selectedImage)
+            val file_extn = filePath!!.substring(filePath!!.lastIndexOf(".") + 1)
+            //image_name_tv.setText(filePath)
+            toast("$requestCode/t" + filePath)
+            try {
+                if (file_extn == "img" || file_extn == "jpg" || file_extn == "jpeg" || file_extn == "gif" || file_extn == "png") {
+                    //FINE
+                    if (requestCode == 1)
+                        uploadImage("image", filePath)
+                } else {
+                    //NOT IN REQUIRED FORMAT
+                    toast("NOT IN REQUIRED FORMAT")
+                }
+            } catch (e: FileNotFoundException) {
+                // TODO Auto-generated catch block
+                toast("Exception : ${e.message}")
+            }
         }
     }
 
-    private fun getPath(uri: Uri?): String {
-        var path: String? = null
-        val projection = arrayOf(MediaStore.Files.FileColumns.DATA)
-        val cursor = contentResolver.query(uri!!, projection, null, null, null)
+    private fun getPath(uri: Uri?): String? {
+        val projection = arrayOf(MediaColumns.DATA)
+        val cursor = managedQuery(uri, projection, null, null, null)
+        val column_index = cursor
+            .getColumnIndexOrThrow(MediaColumns.DATA)
+        cursor.moveToFirst()
+        val imagePath = cursor.getString(column_index)
+        return cursor.getString(column_index)
+    }
 
-        if (cursor == null) {
-            path = uri.path
-        } else {
-            cursor.moveToFirst();
-            val columnIndex = cursor.getColumnIndexOrThrow(projection[0]);
-            path = cursor.getString(columnIndex);
-            cursor.close();
+    private fun uploadImage(param: String, filePath: String) {
+        //Upload Images
+        val client = OkHttpClient()
+        try {
+            val formBuilder = MultipartBody.Builder().setType(MultipartBody.FORM)
+            formBuilder.addFormDataPart(
+                "Authorization",App.getUserToken(this)
+            )
+            formBuilder.addFormDataPart(
+                "driving_licence", "param.png",
+                RequestBody.create(
+                    MediaType.parse("image/jpeg"),
+                    filePath
+                )
+            )
+
+            val requestBody: RequestBody = formBuilder.build()
+            val request = Request.Builder()
+                .url(App.apiBaseUrl + App.UPLOAD_DOCUMENT)
+                .post(requestBody)
+                .build()
+
+
+
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.e("UPLOADIMAGE", "ERROR")
+                    this@DocumentActivity.toast("Error")
+                }
+
+                @Throws(IOException::class)
+                override fun onResponse(call: Call, response: Response) {
+                    Log.e("UPLOADIMAGE", "asdasa $response");
+                }
+            })
+
+        } catch (e: Exception) {
+            toast("Exception : ${e.message}")
         }
-        return if (path == null || path.isEmpty()) uri.path!! else path
     }
 
 
